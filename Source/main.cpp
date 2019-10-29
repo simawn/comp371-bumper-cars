10f2f35227589a98398231e878209d3b349f41ee
Simon Huang <simon@simonhuang.ca>
2019-10-29 03:49:19 -0400

    Initial setup

---
+++ Source/main.cpp
@@ -0,0 +1,16 @@
+#define GLEW_STATIC 1
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/common.hpp>
+#include <FreeImageIO.h>
+
+using namespace glm;
+using namespace std;
+
+#include "EventManager.h"
+
+int main() {
+	return 0;
+}
